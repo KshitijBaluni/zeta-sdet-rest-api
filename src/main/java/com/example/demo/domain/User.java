40317678f5df749ac34930e17b8f8ae6b96db0d5
Kshitij Baluni <kbaluni@auracompany.com>
2023-06-25 14:25:05 +0530

    added persistence to user domain.

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -2,17 +2,35 @@ package com.example.demo.domain;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * User Domain Object Class.
  *
  * @author kshitijbaluni
  * @since 25 June 2023
  */
-@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor @Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private long id;
+    @Column(columnDefinition = "TEXT")
     private String email;
+    @Column(columnDefinition = "TEXT")
     private String name;
+    @Column(columnDefinition = "TEXT")
     private String gender;
+    @Column(columnDefinition = "TEXT")
     private String status;
 }
