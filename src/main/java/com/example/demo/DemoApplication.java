77a96d0723ab8611975945c99940ce7011ed1e52
Kshitij Baluni <kbaluni@auracompany.com>
2023-06-25 14:18:33 +0530

    added java doc for DemoApplication.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Zeta SDET REST API Demo Application.
+ *
+ * @author kshitijbaluni
+ * @since 25 June 2023
+ */
 @SpringBootApplication
 public class DemoApplication {
 
