01438ddd3174f474fd4ec1643ed7215a049e23b2
Kshitij Baluni <kbaluni@auracompany.com>
2023-06-25 14:41:10 +0530

    added user controller.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,36 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.domain.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+/**
+ * User Rest Controller.
+ *
+ * @author kshitijbaluni
+ * @since 25 June 2023
+ */
 @RestController
 @RequestMapping(value = "/public/v2/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Iterable<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<User> getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public void createUser(@RequestBody User user) {
+        userService.createUser(user);
+    }
 }
