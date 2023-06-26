27a7644542b02f4993ac8af0fd3d2dec6093f26e
Kshitij Baluni <kbaluni@auracompany.com>
2023-06-26 15:12:08 +0530

    updated test for user service.

--- src/test/java/com/example/demo/user/UserTest.java
+++ src/test/java/com/example/demo/user/UserTest.java
@@ -3,17 +3,19 @@ package com.example.demo.user;
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.apache.catalina.Server;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 @SpringBootTest
 public class UserTest {
-    @Autowired
     private UserService userService;
 
-    @Autowired
     private UserRepository userRepository;
-    
+
+    @Autowired
     public UserTest(UserService userService, UserRepository userRepository) {
         this.userService = userService;
         this.userRepository = userRepository;
@@ -22,7 +24,7 @@ public class UserTest {
     @Test
     public void testAllUsers() {
         User user = User.builder().id(1).email("test@email").name("Test").status("Active").gender("male").build();
-        //userService.createUser(user);
+        userService.createUser(user);
         userRepository.save(user);
         Assert.notNull(userRepository.findAll(), "Users are null");
     }
